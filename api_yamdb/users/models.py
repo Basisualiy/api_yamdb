6dbd2e1dcc97c769516417c15cceff220daae215
Vasiliy Yakovlev <Basisualiy01@yandex.ru>
2024-08-26 16:43:25 +0300

    Начальная настройка, и кастомный пользователь.

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+ROLES = [
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор'),
+]
+
+
+class CustomUser(AbstractUser):
+    role = models.CharField('Роль пользователя',
+                            max_length=15,
+                            choices=ROLES,
+                            default='user')
+    bio = models.CharField('Биография',
+                           max_length=1024)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
