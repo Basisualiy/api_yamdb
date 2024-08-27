a66f3498b8dbf13c5cba67ffe5f91678bd8fe234
Кристина Гордийчук <gordiichukchristina@yandex.ru>
2024-08-27 12:19:26 +0300

    Add new file

--- api_yamdb/api/admin.py
+++ api_yamdb/api/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from .models import Categories, Genres, Titles, Reviews, CustomUser
 
-# Register your models here.
+# (｡◕‿◕｡)
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    """Настройки отображения модели Categories в административной панели."""
+
+    list_display = ('name', 'slug', 'created_at', 'updated_at')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+
+@admin.register(Genres)
+class GenresAdmin(admin.ModelAdmin):
+    """Настройки отображения модели Genres в административной панели."""
+
+    list_display = ('name', 'slug', 'created_at', 'updated_at')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+
+@admin.register(Titles)
+class TitlesAdmin(admin.ModelAdmin):
+    """Настройки отображения модели Titles в административной панели."""
+
+    list_display = (
+        'name',
+        'year',
+        'description',
+        'category',
+        'created_at',
+        'updated_at')
+    search_fields = ('name', 'description')
+    list_filter = ('category', 'genre', 'year')
+
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    """Настройки отображения модели Reviews в административной панели."""
+
+    list_display = ('title', 'author', 'score', 'created_at', 'updated_at')
+    search_fields = ('title__name', 'author__username', 'text')
+    list_filter = ('score', 'created_at')
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    """Настройки отображения модели CustomUser в административной панели."""
+
+    list_display = ('username',
+                    'email',
+                    'first_name',
+                    'last_name',
+                    'is_staff',
+                    'is_superuser'
+                    )
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_superuser')
