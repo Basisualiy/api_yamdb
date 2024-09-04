7c98a599ea786b17aa9821c899befcaa2595985f
Vasiliy Yakovlev <Basisualiy01@yandex.ru>
2024-09-04 19:26:01 +0300

    Тесты users все зеленные

--- api_yamdb/users/serializer.py
+++ api_yamdb/users/serializer.py
@@ -1,10 +1,39 @@
 from django.contrib.auth import get_user_model
 from rest_framework import exceptions, serializers
+from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
 
 
 class UsersSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())],
+        required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role',
+        )
+
+    def validate_role(self, value):
+        user = self.root._context['request'].user
+        if user.role != 'admin' and not user.is_superuser:
+            raise exceptions.PermissionDenied(
+                'У вас нет прав на изменение роли.')
+        return value
+
+
+class MeSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())],
+        required=True)
+
     class Meta:
         model = User
         fields = (
